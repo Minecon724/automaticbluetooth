e9ca806a3bc054aff6826df538337b4d3fea9e31
Minecon724 <39800107+Minecon724@users.noreply.github.com>
2018-12-08 13:27:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo Downloading AutomaticBluetooth...
 sudo git clone https://github.com/Minecon724/automaticbluetooth
 sudo pip3 install pexpect
 echo Installing...
-sudo mv home/automaticbluetooth/automaticbluetooth.service /etc/systemd/system/automaticbluetooth.service
+sudo mv /home/automaticbluetooth/automaticbluetooth.service /etc/systemd/system/automaticbluetooth.service
 sudo chmod 644 /etc/systemd/system/automaticbluetooth.service
 sudo systemctl enable automaticbluetooth.service
 echo Done.
