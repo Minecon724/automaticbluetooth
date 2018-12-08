770a5a373b3a962fa4116958002f105260e66c92
Minecon724 <maciekpeczyk@gmail.com>
2018-12-08 13:17:03 +0100

    Initial commit

--- setup.sh
+++ setup.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 cd /home
-echo Downloading SpeakerPi and required packages...
-sudo git clone https://github.com/Minecon724/speakerpi
-sudo apt-get install pulseaudio-module-bluetooth bluez-tools
+echo Downloading AutomaticBluetooth...
+sudo git clone https://github.com/Minecon724/automaticbluetooth
+sudo pip3 install pexpect
 echo Installing...
-sudo mv /speakerpi/speakerpi.service /lib/systemd/system/speakerpi.service
-sudo chmod 644 /lib/systemd/system/speakerpi.service
-sudo gpasswd -a $(whoami) pulse
-sudo gpasswd -a $(whoami) lp
-sudo gpasswd -a $(whoami) audio
-sudo gpasswd -a pulse lp
-sudo gpasswd -a pulse audio
-sudo hciconfig hci0 up
-sudo hciconfig hci0 class 0x200420
-sudo sh -c "echo 'extra-arguments = --exit-idle-time=-1 --log-target=syslog' >> /etc/pulse/client.conf"
-echo Done. Reboot may be needed.
+sudo mv /automaticbluetooth/automaticbluetooth.service /etc/systemd/system/automaticbluetooth.service
+sudo chmod 644 /etc/systemd/system/automaticbluetooth.service
+sudo systemctl enable automaticbluetooth.service
+echo Done.
